1cd018e24dff5ca7df57318f67ce2cbe01cf8665
Vo Minh Thu <thu@hypered.io>
2021-05-03 16:21:17 +0200

    Add a test, fix a bug.

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,20 @@
 
 # This checks the simple Publicodes compensation rules against known values.
 
+@test "simple case 100 EUR" {
+  run ./run-publicodes.sh \
+    --set "revenu brut mensuel" 100
+  [ "$status" -eq 0 ]
+  [ "${lines[0]}" = "revenu brut mensuel: 100 EUR." ]
+  [ "${lines[1]}" = "cotisations sociales personnelles: 13,07 EUR." ]
+  [ "${lines[2]}" = "bonus à l'emploi pour les bas salaires: 13,07 EUR." ]
+  [ "${lines[3]}" = "revenu brut imposable: 100 EUR." ]
+  [ "${lines[4]}" = "précompte professionnel: 0 EUR." ]
+  [ "${lines[5]}" = "réduction du précompte professionnel pour bas salaires: 0 EUR." ]
+  [ "${lines[6]}" = "cotisation spéciale pour la sécurité sociale: 0 EUR." ]
+  [ "${lines[7]}" = "revenu net: 100 EUR." ]
+}
+
 @test "simple case 1800 EUR" {
   run ./run-publicodes.sh
   [ "$status" -eq 0 ]
