a152b20972c31a8b86e06f805e0d85ea3078ed9a
Vo Minh Thu <thu@hypered.io>
2022-03-26 12:40:01 +0100

    Add comment.

--- run-publicodes.sh
+++ run-publicodes.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p nodejs
 
-# This runs the simple compensation formulas written in publicodes.
+# This runs the simple compensation formulas written in publicodes. This
+# assumes some node_modules dependencies (see the .github workflow for an
+# example setup command).
 
 node publicodes/script.mjs "publicodes/compensation.yaml" "$@"
