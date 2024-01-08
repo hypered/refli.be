b697f4c1165f2400b84a3e6034321822874906f2
Vo Minh Thu <thu@hypered.io>
2024-01-08 19:49:42 +0100

    Show additional info in scripts/serve.sh.

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -3,5 +3,7 @@
 
 DIR=$(nix-build --attr public --no-out-link)
 
-echo "You can now visit http://127.0.0.1:9000/."
+echo "Now serving ${DIR}..."
+echo "You can now visit http://127.0.0.1:9000/fr/"
+echo "or http://127.0.0.1:9000/changelog.html."
 httpd -f -p 9000 -h $DIR
